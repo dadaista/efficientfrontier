841a3a2aae41f64e6396acf5b77dbd9a14d99136
Davide <dcarboni@gmail.com>
2016-10-01 01:19:56 +0100

    sp500 best portfolio

--- sp500.R
+++ sp500.R
@@ -26,34 +26,35 @@ for (t in Tickers){
   Sys.sleep(1)
 }
 
-top <- head(df[order(-df$Return),],40)
+df$sharpe <- df$Return / df$Volatility
+df <- df[complete.cases(df),]
+
+top <- df[df$Return>0,]
+#keep top 25% volatility
+top <- top[top$Volatility<quantile(top$Volatility,0.25),]
+top <-head( top[order(-top$sharpe),], 50 )
 top
 
-for(i in 1:3){
-  tickers <- sample(top$Ticker,6)
+top$Ticker <- as.character(top$Ticker)
+bestList=list()
+volat=c()
+for(i in 1:300){
+  tickers <- sample(top$Ticker,5)
   tickers
   prices <- loadMulti(tickers,to.date = 60)
   rets <- as.returns(prices)
   portfolios <- generatePortfolios(rets,250)
   best <- bestPortfolio(portfolios,expected = 0.001)
   best
+  bestList[[i]] <- best
+  if(length(best$sd) != 0)
+    volat <- c(volat,best$sd)
+  else volat <- c(volat,NA)
 }
 
+bestList
+superbest <- bestList[[which.min(volat)]]
+superbest
 
-
-  
-df <- df[order(-df$Return),]
-df
-write.csv(df,"sp500.ret.sd.csv")  
-
-df$Sharpe = df$Return / df$Volatility
-
-
-
-df <- df[order(-df$Sharpe),]
-df
-
-df2 <- df[df$Return>0,][order(-df$RateOfChange),]
-head(df2,10)
 
 
