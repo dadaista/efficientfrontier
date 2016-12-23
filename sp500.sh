bc0ba964fc1d637a0d52c45c6659d932d8af0fa6
Davide <dcarboni@gmail.com>
2016-12-23 19:07:02 +0000

    + bash exe

--- sp500.sh
+++ sp500.sh
@@ -4,7 +4,7 @@
 python2.7 -m SimpleHTTPServer 6161 &
 
 COUNTER=0
-while [  $COUNTER -lt 10 ]; do
+while [  $COUNTER -lt 120 ]; do
  Rscript sp500.R
  python2.7 csv2html.py top.csv > top.html
  echo The counter is $COUNTER
