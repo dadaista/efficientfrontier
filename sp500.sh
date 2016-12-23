d614e19bbe2ba769e7f55fd48b43c677f5cb43a5
Davide <dcarboni@gmail.com>
2016-12-23 18:51:14 +0000

    + bash

---
+++ sp500.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/docker/frontieR
+
+python2.7 -m SimpleHTTPServer 6161 &
+
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
+ Rscript sp500.R
+ python2.7 csv2html.py top.csv > top.html
+ echo The counter is $COUNTER
+ sleep 7200
+ let COUNTER=COUNTER+1 
+done
