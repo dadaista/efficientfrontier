35845c5793ee8f7e2c1b555d0c77289f6f363398
Davide <dcarboni@gmail.com>
2016-12-26 16:28:56 +0000

    top.csv

--- sp500.sh
+++ sp500.sh
@@ -6,8 +6,8 @@ python2.7 -m SimpleHTTPServer 6161 &
 COUNTER=0
 while [  $COUNTER -lt 120 ]; do
  git pull
- Rscript sp500.R
  R -e 'rmarkdown::render("bestpicks.Rmd")'
+ Rscript sp500.R
  echo The counter is $COUNTER
  sleep 7200
  let COUNTER=COUNTER+1 
